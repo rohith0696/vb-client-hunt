62846ca3523e4847a390a8e0c6847170a12071e3
Vikas2004 <S538336@NWMISSOURI.EDU>
2021-01-27 02:43:43 -0600

    errors fix

--- scripts/checkLocation.js
+++ scripts/checkLocation.js
@@ -6,23 +6,7 @@ locationsArray = [
     Notes: 'circle',
     Attachments: '',
     'Quest-Location': 'Horizons west'
-},
-    {
-        Name: 'Colden Hall',
-        latitude:40.35112431021874, 
-        longitude:-94.8824199980626,
-        Notes: 'circle',
-        Attachments: '',
-        'Quest-Location': 'Colden Hall'
-      },  
-  {
-        Name: 'Bearcat Stadium',
-        latitude:40.35091480759317, 
-        longitude:-94.88591878401455,
-        Notes: 'circle',
-        Attachments: '',
-        'Quest-Location': 'Bearcat Stadium'
-      }
+}
 ];
   
 
